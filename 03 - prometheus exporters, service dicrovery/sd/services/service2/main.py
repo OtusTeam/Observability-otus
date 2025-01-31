ba19da57c36dfa619419eb04f7ae1d9066f6d488
OtusTeam <46933592+OtusTeam@users.noreply.github.com>
2025-01-31 10:12:05 +0300

    Add files via upload

---
+++ 03 - prometheus exporters, service dicrovery/sd/services/service2/main.py
@@ -0,0 +1,37 @@
+import requests
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+SERVICE_NAME = "service2"
+SERVICE_PORT = 5002
+
+@app.route('/metrics')
+def metrics():
+    value = random.randint(0, 100)
+    return f'test_metric{{service="{SERVICE_NAME}"}} {value}\n'
+
+def register_to_consul():
+    consul_url = "http://consul:8500/v1/agent/service/register"
+    payload = {
+        "Name": SERVICE_NAME,
+        "Address": "service2",
+        "Port": SERVICE_PORT,
+        "Check": {
+            "HTTP": f"http://service2:5002/metrics",
+            "Interval": "10s"
+        }
+    }
+    try:
+        response = requests.put(consul_url, json=payload)
+        if response.status_code == 200:
+            print(f"{SERVICE_NAME} зарегистрирован в Consul")
+        else:
+            print(f"Ошибка регистрации в Consul: {response.status_code}, {response.text}")
+    except Exception as e:
+        print(f"Не удалось зарегистрировать {SERVICE_NAME} в Consul: {e}")
+
+if __name__ == '__main__':
+    register_to_consul()
+    app.run(host='0.0.0.0', port=5002)
